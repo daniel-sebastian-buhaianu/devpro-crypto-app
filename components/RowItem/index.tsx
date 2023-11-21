7b060df3eef3cb0697fb3b79d50be581f6e02467
Zenia Dippenaar <zeniadippenaar@gmail.com>
2023-11-21 07:38:46 +0200

    Add formatting price util function, and fix deprecated config warning

--- components/RowItem/index.tsx
+++ components/RowItem/index.tsx
@@ -7,8 +7,13 @@ import CoinPriceChart from "@/components/CoinPriceChart";
 import { Coin } from "@/types";
 import formatNumber from "@/utils/formatNumber";
 import getPercentage from "@/utils/getPercentage";
+import getFormattedPrice from "@/utils/getFormattedPrice";
 
 const RowItem = ({coin, currency}: {coin: Coin, currency: string}) => {
+  const priceChange1h: number = getFormattedPrice(coin.price_change_percentage_1h_in_currency);
+  const priceChange24h: number = getFormattedPrice(coin.price_change_percentage_24h_in_currency);
+  const priceChange7d: number = getFormattedPrice(coin.price_change_percentage_7d_in_currency);
+
   return (
     <div className="dark:bg-blackberry bg-white mb-2 p-5 rounded-lg flex items-center text-sm">
       <span className="mr-2 w-[3%] text-center">{coin.market_cap_rank}</span>
@@ -21,13 +26,13 @@ const RowItem = ({coin, currency}: {coin: Coin, currency: string}) => {
         <span className="ml-1">{formatNumber(coin.current_price)}</span>
       </span>
       <span className="w-[7%] px-1">
-        <PriceChange price={parseFloat(coin.price_change_percentage_1h_in_currency.toFixed(2))} />
+        <PriceChange price={priceChange1h} />
       </span>
       <span className="w-[7%] px-1">
-        <PriceChange price={parseFloat(coin.price_change_percentage_24h_in_currency.toFixed(2))} />
+        <PriceChange price={priceChange24h} />
       </span>
       <span className="w-[7%] px-1">
-        <PriceChange price={parseFloat(coin.price_change_percentage_7d_in_currency.toFixed(2))} />
+        <PriceChange price={priceChange7d} />
       </span>
       <div className="w-full max-w-[18%] px-1">
         <div className="flex justify-between text-xs">
@@ -44,7 +49,7 @@ const RowItem = ({coin, currency}: {coin: Coin, currency: string}) => {
         <MarketVolumeBar fill="bg-apricot" percentage={getPercentage(coin.circulating_supply, coin.total_supply)}/>
       </div>
       <div className="w-full max-w-[14%] pl-3 text-center h-[50px]">
-        <CoinPriceChart prices={coin.sparkline_in_7d.price} priceChange={parseFloat(coin.price_change_percentage_7d_in_currency.toFixed(2))}/>
+        <CoinPriceChart prices={coin.sparkline_in_7d.price} priceChange={priceChange7d}/>
       </div>
     </div>
   )
